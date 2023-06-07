0f502252586b9719215c4ac1dbe8f9bc17984936
henok1111 <basahenok@gmail.com>
2023-06-07 16:46:56 +0300

    final gr4 assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,167 +1,22 @@
 import 'package:flutter/material.dart';
+import 'userinput.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'User Registration',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: RegistrationForm(),
-    );
-  }
-}
-
-class RegistrationForm extends StatefulWidget {
-  @override
-  _RegistrationFormState createState() => _RegistrationFormState();
-}
-
-class _RegistrationFormState extends State<RegistrationForm> {
-  final _formKey = GlobalKey<FormState>();
-
-  late String _firstName;
-  late String _lastName;
-  late String _email;
-  late String _password;
-  late String _rePassword;
-  bool _passwordVisible = false;
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      // Perform registration or user creation logic here
-      print('Creating user...');
-      print('First Name: $_firstName');
-      print('Last Name: $_lastName');
-      print('Email: $_email');
-      print('Password: $_password');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('User Registration'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'First Name',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'First name is required';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _firstName = value!;
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Last Name',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Last name is required';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _lastName = value!;
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                ),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Email is required';
-                  }
-                  if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-                      .hasMatch(value)) {
-                    return 'Please enter a valid email address';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _email = value!;
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: _passwordVisible
-                        ? Icon(Icons.visibility_off)
-                        : Icon(Icons.visibility),
-                    onPressed: () {
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    },
-                  ),
-                ),
-                obscureText: !_passwordVisible,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Password is required';
-                  }
-                  if (value.length < 8) {
-                    return 'Password must be at least 8 characters long';
-                  }
-                  if (!RegExp(r'^(?=.*?[!@#\$&*~]).{8,}$').hasMatch(value)) {
-                    return 'Password must contain at least one special character';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _password = value!;
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Re-enter Password',
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please re-enter the password';
-                  }
-                  if (value != _password) {
-                    return 'Passwords do not match';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _rePassword = value!;
-                },
-              ),
-              SizedBox(height: 24.0),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: Text('Create User'),
-              ),
-            ],
-          ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('User Input Forms'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: UserInputForms(),
         ),
       ),
     );
---
+++ lib/userinput.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class UserInputForms extends StatefulWidget {
+  @override
+  _UserInputFormsState createState() => _UserInputFormsState();
+}
+
+class _UserInputFormsState extends State<UserInputForms> {
+  final _formKey = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _retypePasswordController = TextEditingController();
+  bool _obscurePassword = true;
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _retypePasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _firstNameController,
+            decoration: const InputDecoration(
+              labelText: 'First Name',
+            ),
+          ),
+          TextFormField(
+            controller: _lastNameController,
+            decoration: const InputDecoration(
+              labelText: 'Last Name',
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'last name is required ';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(
+              labelText: 'Email',
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'email is required';
+              }
+              if (!isEmailValid(value)) {
+                return ' invalid email type';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              suffixIcon: IconButton(
+                icon: Icon(
+                  _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscurePassword = !_obscurePassword;
+                  });
+                },
+              ),
+            ),
+            obscureText: _obscurePassword,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your password';
+              }
+              if (!isPasswordValid(value)) {
+                return 'Password must be at least 8 characters long and contain a combination of uppercase letters, lowercase letters, and numbers';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _retypePasswordController,
+            decoration: const InputDecoration(
+              labelText: 'Retype Password',
+            ),
+            obscureText: true,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please re-enter your password';
+              }
+              if (value != _passwordController.text) {
+                return 'Passwords is not match';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // Form is valid, perform necessary actions
+                // For example, save the user's input to a database
+                // or navigate to the next screen
+              }
+            },
+            child: const Text('Create User'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isEmailValid(String email) {
+    // A simple email validation using a regular expression
+    const emailRegex =
+        r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,})$';
+    final regExp = RegExp(emailRegex);
+    return regExp.hasMatch(email);
+  }
+
+  bool isPasswordValid(String password) {
+    // Password validation for at least 8 characters with a combination of uppercase letters, lowercase letters, and numbers
+    const passwordRegex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$';
+    final regExp = RegExp(passwordRegex);
+    return regExp.hasMatch(password);
+  }
+}
